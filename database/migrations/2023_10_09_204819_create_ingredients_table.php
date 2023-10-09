4c73964a1a5d68b195faf02216350758b8aa4c40
Hossam Tarek <hossamtarek250@gmail.com>
2023-10-09 23:58:50 +0300

    Define database structure

---
+++ database/migrations/2023_10_09_204819_create_ingredients_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ingredients', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('initial_stock'); // In grams
+            $table->integer('current_stock'); // In grams
+            $table->boolean('merchant_notified')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ingredients');
+    }
+};
